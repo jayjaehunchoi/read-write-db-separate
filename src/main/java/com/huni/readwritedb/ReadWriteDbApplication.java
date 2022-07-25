73d74c32cd9489ffddef1118169019819d76b65e
jayjaehunchoi <wogns0108@naver.com>
2022-07-25 12:24:19 +0900

    initial commit

---
+++ src/main/java/com/huni/readwritedb/ReadWriteDbApplication.java
@@ -0,0 +1,13 @@
+package com.huni.readwritedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWriteDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadWriteDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huni/readwritedb/ReadWriteDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huni.readwritedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadWriteDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
